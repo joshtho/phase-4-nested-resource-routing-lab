ba2a0d248c11ed97f4717c519c451dccb401c814
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2023-01-30 19:20:53 -0600

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  resuce_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
     if params[:user_id]
@@ -8,6 +9,27 @@ class ItemsController < ApplicationController
     items = Item.all
     end
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(items_params)
+    render json: item, status: :created
+  end
+
+  private
+
+  def items_params
+    params.permit(:name, :description, :price)
+  end
+
+  def render_not_found_response(exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
   end
 
 end
